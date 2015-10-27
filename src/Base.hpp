79107cd5826ff7f512564e94fecb6b78e9db1f3a
Candice Bentejac <cbent002@hammer.cs.ucr.edu>
2015-10-27 14:50:05 -0700

    Added Base.hpp, the base class. Included Base.hpp in rshell.cpp and added exits in case syscalls fail.

---
+++ src/Base.hpp
@@ -0,0 +1,13 @@
+// Base file; others files will inherit from it
+
+#ifndef _BASE_HPP_
+#define _BASE_HPP_
+
+#include <iostream>
+#include <string.h>
+#include <unistd.h> // execvp, fork, waitpid, getlogin, gethostname
+#include <stdio.h> // perror
+#include <stdlib.h> // exit
+
+
+#endif
